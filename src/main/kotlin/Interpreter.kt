4b72ef920423adbc4fbd557cc026a3add46d5366
flyingV805 <petrov.vladislav1992@gmail.com>
2024-07-27 14:27:29 +0500

    Input operator

--- src/main/kotlin/Interpreter.kt
+++ src/main/kotlin/Interpreter.kt
@@ -1,8 +1,57 @@
 class Interpreter(
-    private val program: List<Operator>
+    private val program: List<Operator>,
+    private val memory: Array<Byte>
 ){
 
+    private var instructionIndex = 0
+    private var memoryPointer = 0
+    private var loopStartIndex: Int? = null
+    private var loopEndIndex: Int? = null
+
     fun run(){
+
+        while (instructionIndex < program.size) {
+            when(program[instructionIndex]){
+                Forward -> memoryPointer++
+                Backward -> memoryPointer--
+                Increment -> memory[memoryPointer]++
+                Decrement -> memory[memoryPointer]--
+                Output -> print(memory[memoryPointer].toInt().toChar())
+                Input -> {
+                    readlnOrNull()?.first()?.code?.toByte()?.let {
+                        memory[memoryPointer] = it
+                    }
+                }
+                StartLoop -> {
+                    loopStartIndex = instructionIndex
+                    if( memory[memoryPointer] == 0x00.toByte() ){
+
+                        //have loop end index, no need to search
+                        if(loopEndIndex != null){
+                            instructionIndex = loopEndIndex!!
+                            continue
+                        }
+
+                        //have loop end index, no need to search
+                        for (le in instructionIndex until program.size){
+                            if( program[le] == EndLoop) {
+                                instructionIndex = le
+                                continue
+                            }
+                        }
+                    }
+                }
+                EndLoop -> {
+                    loopEndIndex = instructionIndex
+                    if( memory[memoryPointer] != 0x00.toByte() ){
+                        instructionIndex = loopStartIndex!!
+                        continue
+                    }
+                }
+            }
+
+            instructionIndex++
+        }
 
     }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,54 +1,9 @@
 
 fun main(args: Array<String>) {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
-
-    val program = Parser().parseProgram(Test.helloWorld)
+    val program = Parser().parseProgram(Test.echo)
     val memory = Array<Byte>(30000, init = {0x00})
 
-    var instructionIndex = 0
-    var memoryPointer = 0
-    var loopStartIndex: Int? = null
-    var loopEndIndex: Int? = null
-
-    //event loop ? (lol)
-    while (instructionIndex < program.size) {
-        when(program[instructionIndex]){
-            Forward -> memoryPointer++
-            Backward -> memoryPointer--
-            Increment -> memory[memoryPointer]++
-            Decrement -> memory[memoryPointer]--
-            Output -> print(memory[memoryPointer].toInt().toChar())
-            Input -> {}
-            StartLoop -> {
-                loopStartIndex = instructionIndex
-                if( memory[memoryPointer] == 0x00.toByte() ){
-
-                    if(loopEndIndex != null){
-                        instructionIndex = loopEndIndex
-                        continue
-                    }
-
-                    for (le in instructionIndex until program.size){
-                        if( program[le] == EndLoop) {
-                            instructionIndex = le
-                            continue
-                        }
-                    }
-                }
-            }
-            EndLoop -> {
-                loopEndIndex = instructionIndex
-                if( memory[memoryPointer] != 0x00.toByte() ){
-                    instructionIndex = loopStartIndex!!
-                    continue
-                }
-            }
-        }
-
-        instructionIndex++
-    }
+    Interpreter(program, memory).run()
 
 }
